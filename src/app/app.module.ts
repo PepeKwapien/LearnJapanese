94b03ef0b0d48493e468f1541f7e37dce773d7b0
PepeKwapien <pppkwapien@gmail.com>
2022-09-15 14:49:56 +0200

    Added template button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ButtonWithIconComponent } from './components/button-with-icon/button-with-icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ButtonWithIconComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/button-with-icon/button-with-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-with-icon',
+  templateUrl: './button-with-icon.component.html',
+  styleUrls: ['./button-with-icon.component.css'],
+})
+export class ButtonWithIconComponent implements OnInit {
+  @Input() text: string = 'Click me';
+  @Input() disabled: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
