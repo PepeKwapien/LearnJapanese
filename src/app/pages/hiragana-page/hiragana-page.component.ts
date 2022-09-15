6c34d55dc970dcb70863781beb2eb4d70819dbe8
PepeKwapien <pppkwapien@gmail.com>
2022-09-15 22:50:49 +0200

    Hiragana page uses class fields

--- src/app/pages/hiragana-page/hiragana-page.component.ts
+++ src/app/pages/hiragana-page/hiragana-page.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hiragana-page',
   templateUrl: './hiragana-page.component.html',
-  styleUrls: ['./hiragana-page.component.css']
+  styleUrls: ['./hiragana-page.component.css'],
 })
 export class HiraganaPageComponent implements OnInit {
+  syllabary: string = 'Hiragana';
+  monographs: number = 45;
+  digraphs: number = 21;
+  diacritics: number = 25;
+  description: string =
+    'It is a phonetic lettering system. The word hiragana literally means "flowing" or "simple" kana ("simple" originally as contrasted with kanji).';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
