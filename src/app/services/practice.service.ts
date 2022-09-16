ed300f42d0c306bc7c1e950150cad31e0ca88302
PepeKwapien <pppkwapien@gmail.com>
2022-09-16 13:09:53 +0200

    Added practice service

---
+++ src/app/services/practice.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { PracticeModes } from '../enums/practice-modes';
+import { PracticeFormModel } from '../interfaces/practice-form-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PracticeService {
+  private defaultModel: PracticeFormModel = {
+    mode: PracticeModes.characters,
+    digraphs: false,
+    diacritics: false,
+    digraphsdiacritics: false,
+  };
+  private model: PracticeFormModel;
+
+  constructor() {
+    this.resetModel();
+  }
+
+  setModel(model: PracticeFormModel) {
+    this.model = model;
+  }
+
+  getModel() {
+    return this.model;
+  }
+
+  resetModel() {
+    this.model = this.defaultModel;
+  }
+}
